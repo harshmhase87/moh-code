5ec7968539f3bfaf0d325b79611f9949ebfa606d
harsh mhase <harshmhase87@gmail.com>
2025-10-28 16:56:13 +0530

    fixed host and port for Render deployment

--- app1.py
+++ app1.py
@@ -12,4 +12,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
